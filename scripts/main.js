641478ba9eed163cd7ed1f0e18790ac5c2bd7e43
Nataliya Satsyuk <natalia.satsyuk@gmail.com>
2016-08-06 23:13:19 +0300

    initialization

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+$(".header-menu-bars").click(function() {
+	if ($(document).width() > 768) {
+		$(".list").fadeToggle(400);
+	} else {
+		$(".list").slideToggle(400);
+	}
+	
+});
+
+$(".header-menu-search").click(function() {
+	$("form").fadeToggle(400);
+});
+
