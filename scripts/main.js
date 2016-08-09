08ce47173357cfef567ca09c3ba665bb6dd37504
Nataliya Satsyuk <natalia.satsyuk@gmail.com>
2016-08-09 15:58:17 +0300

    second slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
+
+// menu
 $(".header-menu-bars").click(function() {
 	if ($(document).width() > 768) {
 		$(".list").fadeToggle(400);
 	} else {
 		$(".list").slideToggle(400);
 	}
-	
 });
 
 $(".header-menu-search").click(function() {
@@ -12,8 +13,19 @@ $(".header-menu-search").click(function() {
 });
 
 
-
-// gallery
+// gallery 3 pictures
 $(function() {
 	$('#dg-container').gallery();
 });
+
+
+// gallery picture + text
+$('.container-person').slick({
+	dots: false,
+	// prevArrow: '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
+	// nextArrow: '<i class="fa fa-chevron-right" aria-hidden="true"></i>'
+});
+
+
+
+// 
